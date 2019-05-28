34c3ade0528deae55d78e289a9186ee2c0d9ddd0
Joshua Olivares <joshuaolivares@utexas.edu>
2019-05-28 20:49:19 +0000

    init repos

---
+++ lib/olive_palindrome.rb
@@ -0,0 +1,6 @@
+require "olive_palindrome/version"
+
+module OlivePalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/olive_palindrome/version.rb
@@ -0,0 +1,3 @@
+module OlivePalindrome
+  VERSION = "0.1.0"
+end
